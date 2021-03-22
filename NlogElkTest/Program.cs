7e75575760c0b2ee16aa2407ac5b840d7f472e62
yanghy <yanghy@fxtcn.com>
2021-03-22 18:25:24 +0800

    添加项目文件。

---
+++ NlogElkTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NlogElkTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
